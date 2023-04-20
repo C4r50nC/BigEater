7f4beb52dc180b551b63d1785ad4f3589e082a50
C4r50nC <xcch7@student.unimelb.edu.au>
2023-04-20 21:13:07 +1000

    feat: add index page

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  res.redirect("player");
+});
+
+app.get("/player", (req, res) => {
   res.render("player");
 });
 
